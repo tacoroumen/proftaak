fdb8dcfd2392baa504dd823422e73b1d0f66fbab
Tapardy <117444657+Tapardy@users.noreply.github.com>
2022-11-15 21:09:48 +0100

    updated arduino code

--- Tech/Arduino weight sensor/Arduino weight sensor.ino
+++ Tech/Arduino weight sensor/Arduino weight sensor.ino
@@ -1,13 +1,6 @@
 #include <HX711.h>
 #include <HX711_ADC.h>
 
-
-/*NOTES
-total wordt groter als de price boven total komt, anders niet. De massa wordt hier niet goed gerekend.
-Dit is een probleem want de treshold wordt steeds groter en groter.
-Niet functioneel zoals het moet werken.
-*/
-
 HX711 scale;
 const int CLOCKPIN = 4;
 const int DATAPIN = 5; 
@@ -16,22 +9,22 @@ int i = 0;
 double pricearray[ARRAYLENGTH] = {};
 double price = 0;
 double weight = 0;
-double total = 0;
+double total_weight = 0;
+double total_price = 0;
 unsigned long t = 0;
+String value;
+
 HX711_ADC LoadCell(DATAPIN, CLOCKPIN);
-unsigned long time_1 = 0;
-
-
-int period = 500;
-unsigned long time_now = 0;
 
 void setup(){
   Serial.begin(57600);
   scale.begin(DATAPIN, CLOCKPIN);
   scale.tare();
-  Serial.println("Please remove all mass from the scale");
-  delay(3000);
   LoadCell.begin();
+  Serial.println("Please remove all mass from the scale");
+  Serial.print('\n');
+  delay(3000);
+
   calibrate(); //start calibration
 }
 
@@ -39,71 +32,81 @@ void setup(){
 
 void calibrate() {
   Serial.println("Calibration start");
-
-  time_now = millis();
-
-  while(millis() < time_now + period){
-    int random = 1 + (rand() % 3);
-    if (random = 1){
-      Serial.print("-----");
-    }else if (random = 2){
-      Serial.print("---");
-    }else if (random = 3){
-      Serial.print("-");
-    }
-  }
-  // delay(500);
-  // Serial.print("-----");
-  // delay(500);
-  // Serial.print("-----");
-  // delay(500);
-  // Serial.print("-----");
-  // delay(500);
-  // Serial.print("-----");
   LoadCell.refreshDataSet(); //refresh the dataset to be sure that the known mass is measured correct
   float newCalibrationValue = 459.36;
   scale.set_scale(newCalibrationValue); 
-  delay(1500);
-  Serial.println();
-  Serial.println("Calibration finished");
   delay(500);
-  Serial.println("Mass can now be placed");
+  Serial.println("Calibration finished");
+  delay(1000);
+  Serial.print('\n');
+  Serial.println("Please put the object you want to measure on the scale");
 }
 
 void priceofplastic(){
   price = weight / 1000 * 0.75;
-  Serial.println();
+  Serial.print('\n');
   Serial.print("price: ");
   Serial.print(price);
   Serial.print(" euro");
-
-  pricearray[i] = price;
-
-  if (pricearray[i] > pricearray[i - 1]){
-    total = total + pricearray[i];     
-    i++;
-  }
-  if (i = 10){
-    i = 0;
-  }
-  Serial.println();
-  Serial.print("Total: ");
-  Serial.print(total);
+  Serial.print('\n');
+  
+  total_price = total_price + price;     
+  Serial.print("Total price: ");
+  Serial.print(total_price);
   Serial.print(" euro");
-  Serial.println("");
+  Serial.println('\n');
 }
 
-void loop(){
+
+void measurements(){
   weight = scale.get_units(10);
-  delay(500);        
-  if (weight > 0){
-    Serial.println("");
-    Serial.print("Weight: ");   
-    Serial.print(weight); 
-    Serial.print(" gram");
-    priceofplastic();
+  total_weight = total_weight + weight;  
+  Serial.print('\n');
+  Serial.print("Weight: ");   
+  Serial.print(weight); 
+  Serial.print(" grams");
+  Serial.print('\n'); 
+
+  Serial.print("Total weight: ");
+  Serial.print(total_weight);
+  Serial.print(" grams");
+
+  priceofplastic(); //measure and print the price of the measured object
+
+  delay(1500);
+  Serial.println("Please remove said object from the scale");
+  delay(3000);
+  Serial.print('\n');
+
+  serialread();
+}
+void serialread(){
+  Serial.println("Do you wish to continue measuring?");
+  Serial.println("Send 'yes' if you wish to continue and 'no' if you wish to stop");
+
+  if (Serial.available() > 0){
+    char received = Serial.read();
+    if (received == '\n'){
+      if (value == "yes"){
+        Serial.println("yes");
+        value = "";
+      }
+      if (value == "no"){
+        Serial.println("no");
+        value = "";
+      }
+    }else {
+      value += received; 
+    }
+  } 
+}
+
+
+void loop(){
+  weight = scale.get_units(10);      
+  if (weight > 1){
+    measurements(); //Function to measure weight once.
   }
-  delay(500);
 }
 
 
