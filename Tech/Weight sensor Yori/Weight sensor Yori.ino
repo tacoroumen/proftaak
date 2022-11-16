8e6492308d1b37a9f88a5d555ef0aedc59d01d64
Tortar <tacoroumen@gmail.com>
2022-11-16 14:34:25 +0100

    arduino update

---
+++ Tech/Weight sensor Yori/Weight sensor Yori.ino
@@ -0,0 +1,110 @@
+#include <HX711.h>
+#include <HX711_ADC.h>
+
+HX711 scale;
+const int CLOCKPIN = 4;
+const int DATAPIN = 5; 
+double weight = 0;
+double total_weight = 0;
+double price = 0;
+double total_price = 0;
+String message;
+HX711_ADC LoadCell(DATAPIN, CLOCKPIN);
+
+void setup(){
+  Serial.begin(57600);
+  scale.begin(DATAPIN, CLOCKPIN);
+  scale.tare();
+  LoadCell.begin();
+  Serial.println("Please remove all mass from the scale");
+  Serial.print('\n');
+  delay(3000);
+  calibrate(); //start the calibration process
+}
+
+void serialreadmeasure(){
+  if (Serial.available() > 0) {
+    char received = Serial.read();
+    
+    if (received == '\n') {
+      if (message == "yes"){
+        Serial.println("Please put the object you want to measure on the scale");
+      }
+      else if (message == "no") {
+        Serial.println("Thank you for using this!");
+        Serial.print("Your total money back is: ");
+        Serial.print(total_price);
+        Serial.print(" euro");
+        Serial.print('\n');
+        Serial.print("The total weight of the measured objects is: ");
+        Serial.print(total_weight / 1000);
+        Serial.print(" kilograms");
+        //print receipt for customer
+      }
+      message = ""; 
+    }else {
+      message += received;
+    }
+  }
+}
+
+void calibrate() {
+  Serial.println("Calibration start");
+  LoadCell.refreshDataSet(); //refresh the dataset to be sure that the known mass is measured correct
+  float newCalibrationValue = 459.36;
+  scale.set_scale(newCalibrationValue); 
+  delay(500);
+  Serial.println("Calibration finished");
+  delay(1000);
+  Serial.print('\n');
+  Serial.println("Please put the object you want to measure on the scale");
+}
+
+void priceofplastic(){
+  price = weight / 1000 * 0.75;
+  Serial.print('\n');
+  Serial.print("price: ");
+  Serial.print(price);
+  Serial.print(" euro");
+  Serial.print('\n');
+  
+  total_price = total_price + price;     
+  Serial.print("Total price: ");
+  Serial.print(total_price);
+  Serial.print(" euro");
+  Serial.println('\n');
+}
+
+void measurements(){
+  weight = scale.get_units(10);
+  total_weight = total_weight + weight;  
+  Serial.print('\n');
+  Serial.print("Weight: ");   
+  Serial.print(weight); 
+  Serial.print(" grams");
+  Serial.print('\n'); 
+
+  Serial.print("Total weight: ");
+  Serial.print(total_weight);
+  Serial.print(" grams");
+
+  priceofplastic(); //measure and print the price of the measured object
+
+  delay(1500);
+  Serial.println("Please remove said object from the scale");
+  delay(3000);
+  Serial.print('\n');
+
+  Serial.println("Do you wish to continue measuring?");
+  Serial.println("Send 'yes' if you wish to continue and 'no' if you wish to stop");
+  Serial.print('\n');
+}
+
+void loop(){
+  weight = scale.get_units(10);      
+  if (weight > 1){
+    measurements(); //Function to measure weight once.
+  }else {
+    serialreadmeasure(); //read user input if they want to continue measuring
+  }
+}
--- Tech/mqtt arduino/src/main.cpp
+++ Tech/mqtt arduino/src/main.cpp
@@ -1,18 +1,212 @@
-#include "include.cpp"
-#include "functions.cpp"
+//#include "include.h"
+//#include "functions.h"
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HX711.h>
+#include <HX711_ADC.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+// initialize scale
+HX711 scale;
+const int CLOCKPIN = 4;
+const int DATAPIN = 5; 
+HX711_ADC LoadCell(DATAPIN, CLOCKPIN);
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "NETLAB-OIL010";
+const char* password = "DesignChallenge";
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "192.168.160.210"; 
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+const int ledPin = 2;
+
+//scale variables
+float price = 0;
+float weight = 0;
+float total_weight = 0;
+float total_price = 0;
+
+//string for serial monitor
+String value;
+
+void calibrate() {
+  Serial.println("Calibration start");
+  LoadCell.refreshDataSet(); //refresh the dataset to be sure that the known mass is measured correct
+  float newCalibrationValue = 459.36;
+  scale.set_scale(newCalibrationValue); 
+  delay(500);
+  Serial.println("Calibration finished");
+  delay(1000);
+  Serial.println("Please put the object you want to measure on the scale");
+}
+
+void priceofplastic(){
+  price = weight / 1000 * 0.75;
+  Serial.print('\n');
+  Serial.print("price: ");
+  Serial.print(price);
+  Serial.print(" euro");
+  Serial.print('\n');
+  
+  total_price = total_price + price;     
+  Serial.print("Total price: ");
+  Serial.print(total_price);
+  Serial.print(" euro");
+  Serial.println('\n');
+}
+
+void serialread(){
+  Serial.println("Do you wish to continue measuring?");
+  Serial.println("Send 'yes' if you wish to continue and 'no' if you wish to stop");
+
+  if (Serial.available() > 0){
+    char received = Serial.read();
+    if (received == '\n'){
+      if (value == "yes"){
+        Serial.println("yes");
+        value = "";
+      }
+      if (value == "no"){
+        Serial.println("no");
+        value = "";
+      }
+    }else {
+      value += received; 
+    }
+  } 
+}
+
+void measurements(){
+  weight = scale.get_units(10);
+  total_weight = total_weight + weight;  
+  Serial.print('\n');
+  Serial.print("Weight: ");   
+  Serial.print(weight); 
+  Serial.print(" grams");
+  Serial.print('\n'); 
+
+  Serial.print("Total weight: ");
+  Serial.print(total_weight);
+  Serial.print(" grams");
+
+  priceofplastic(); //measure and print the price of the measured object
+
+  delay(1500);
+  Serial.println("Please remove said object from the scale");
+  delay(3000);
+  Serial.print('\n');
+
+  serialread();
+}
+
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "proftaak/fontys/led") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client", "proftaak", "04juLi2003!")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("proftaak/fontys/led");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void mqqt_update() {
+    char currentstring[8];
+    float weight_kg = weight/1000;
+    dtostrf(weight_kg, 1, 2, currentstring);
+    Serial.print("Weight in kg: ");
+    Serial.println(currentstring);
+    client.publish("proftaak/fontys/weight", currentstring);
+
+    currentstring[8];
+    float total_weight_kg = total_weight/1000;
+    dtostrf(total_weight_kg, 1, 2, currentstring);
+    Serial.print("Total weight in kg: ");
+    Serial.println(currentstring);
+    client.publish("proftaak/fontys/total_weight", currentstring);
+
+    dtostrf(price, 1, 2, currentstring);
+    Serial.print("price in euro: ");
+    Serial.println(currentstring);
+    client.publish("proftaak/fontys/price", currentstring);
+
+    dtostrf(total_price, 1, 2, currentstring);
+    Serial.print("Total price in euro: ");
+    Serial.println(currentstring);
+    client.publish("proftaak/fontys/total_price", currentstring);
+}
 
 void setup() {
   Serial.begin(115200);
   scale.begin(DATAPIN, CLOCKPIN);
   scale.tare();
   LoadCell.begin();
-  Serial.println("Please remove all mass from the scale, when all mass is removed press enter."); 
-  char received = Serial.read();
-  while (received != '\n'){
-    char received = Serial.read();
-  }
+  setup_wifi();
   calibrate(); //start calibration
-  setup_wifi(); 
   client.setServer(mqtt_server, 1883); //set mqtt server
   client.setCallback(callback);
 }
