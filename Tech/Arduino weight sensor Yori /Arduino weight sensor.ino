70010f8036b42a2bcaaa90971790395625d15ec1
Tapardy <117444657+Tapardy@users.noreply.github.com>
2022-11-16 10:50:06 +0100

    Update Arduino weight sensor.ino

--- Tech/Arduino weight sensor Yori /Arduino weight sensor.ino
+++ Tech/Arduino weight sensor Yori /Arduino weight sensor.ino
@@ -4,16 +4,12 @@
 HX711 scale;
 const int CLOCKPIN = 4;
 const int DATAPIN = 5; 
-const int ARRAYLENGTH = 10;
 int i = 0;
-double pricearray[ARRAYLENGTH] = {};
-double price = 0;
 double weight = 0;
 double total_weight = 0;
+double price = 0;
 double total_price = 0;
-unsigned long t = 0;
-String value;
-
+String message;
 HX711_ADC LoadCell(DATAPIN, CLOCKPIN);
 
 void setup(){
@@ -24,11 +20,34 @@ void setup(){
   Serial.println("Please remove all mass from the scale");
   Serial.print('\n');
   delay(3000);
-
-  calibrate(); //start calibration
+  calibrate(); //start the calibration process
 }
 
+void serialreadmeasure(){
+  if (Serial.available() > 0) {
+    char received = Serial.read();
 
+    if (received == '\n') {
+      if (message == "yes"){
+        Serial.println("Please put the object you want to measure on the scale");
+      }
+      else if (message == "no") {
+        Serial.println("Thank you for using this!");
+        Serial.print("Your total money back is: ");
+        Serial.print(total_price);
+        Serial.print(" euro");
+        Serial.print('\n');
+        Serial.print("The total weight of the measured objects is: ");
+        Serial.print(total_weight / 1000);
+        Serial.print(" kilogramme");
+        //print receipt for customer
+      }
+    message = ""; 
+    }else {
+      message += received;
+    }
+  }
+}
 
 void calibrate() {
   Serial.println("Calibration start");
@@ -78,27 +97,9 @@ void measurements(){
   delay(3000);
   Serial.print('\n');
 
-  serialread();
-}
-void serialread(){
   Serial.println("Do you wish to continue measuring?");
   Serial.println("Send 'yes' if you wish to continue and 'no' if you wish to stop");
-
-  if (Serial.available() > 0){
-    char received = Serial.read();
-    while (received != '\n'){
-      if (value == "yes"){
-        Serial.println("yes");
-        value = "";
-      }
-      if (value == "no"){
-        Serial.println("no");
-        value = "";
-      }
-    value += received; 
-    received = Serial.read();
-    }
-  } 
+  Serial.print('\n');
 }
 
 
@@ -106,8 +107,8 @@ void loop(){
   weight = scale.get_units(10);      
   if (weight > 1){
     measurements(); //Function to measure weight once.
+  }else {
+    serialreadmeasure(); //read user input if they want to continue measuring
   }
+
 }
-
-
-//0.75 cent per kilo
